ad8b2e1e0e72977d20990577b2295e0253e21e8e
EuigunJung <63629484+EuigunJung@users.noreply.github.com>
2022-02-08 18:01:50 -0700

    This is the Quadrant View file V1. Euigun Jung

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MovieCollection.Models;
+//using MovieCollection.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,14 +23,14 @@ namespace Project1
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-            services.AddDbContext<ApplicationContext>(options =>
-            {
-                options.UseSqlite(Configuration["ConnectionStrings:SqlConnection"]);
-            });
-        }
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.AddControllersWithViews();
+        //    services.AddDbContext<ApplicationContext>(options =>
+        //    {
+        //        options.UseSqlite(Configuration["ConnectionStrings:SqlConnection"]);
+        //    });
+        //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
