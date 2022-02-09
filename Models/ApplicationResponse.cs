1b9efc4b0c391701fd8e96431b4867e008850066
dchin1088 <d.chin1088@gmail.com>
2022-02-09 15:47:45 -0700

    Response

--- Models/ApplicationResponse.cs
+++ Models/ApplicationResponse.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project1.Models
 {
     public class ApplicationResponse
     {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        [Required(ErrorMessage = " Please enter the task.")]
+        public string Task { get; set; }
+        [Required(ErrorMessage = " Please enter the due date.")]
+        public string DueDate { get; set; }
+        [Required(ErrorMessage = " Please enter the quadrant it belongs to.")]
+        public byte Quadrant { get; set; }
+        [Required(ErrorMessage = " Please enter the category.")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = " Please mark if it has been completed.")]
+        public bool Completed { get; set; }
+
+         //Build Foreign Key Relationship
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
     }
 }
